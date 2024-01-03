1a9d8af05db10b35cedc76d16bc12de9b773c380
Shajid Hasan <shajid6063@gmail.com>
2024-01-03 19:46:50 +0600

    fixed score

--- src/objects/score.js
+++ src/objects/score.js
@@ -2,11 +2,11 @@ import k from "../kaboom"
 
 export const makeScore = () => {
     const score = k.make([
-        text(0, { font: 'saptami_arcade', size: 120 }),
-        anchor("center"),
-        pos(width() / 2, 80),
-        fixed(),
-        z(100),
+        k.text(0, { font: 'saptami_arcade', size: 120 }),
+        k.anchor("center"),
+        k.pos(width() / 2, 80),
+        k.fixed(),
+        k.z(100),
         {
             value: 0
         }
