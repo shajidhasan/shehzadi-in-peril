e8c3d31f21bc15925a4eaa9642155868e1c6b7f4
Shajid Hasan <shajid6063@gmail.com>
2024-01-03 19:47:26 +0600

    fixed score

--- src/objects/score.js
+++ src/objects/score.js
@@ -4,7 +4,7 @@ export const makeScore = () => {
     const score = k.make([
         k.text(0, { font: 'saptami_arcade', size: 120 }),
         k.anchor("center"),
-        k.pos(width() / 2, 80),
+        k.pos(k.width() / 2, 80),
         k.fixed(),
         k.z(100),
         {
