ea870223e2c1086bdf81b470ca29a3eae2633f62
Shajid Hasan <shajid6063@gmail.com>
2024-01-04 03:47:13 +0600

    stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [
-        VitePWA({ registerType: 'autoUpdate' })
-    ],
     build: {
         minify: 'terser'
     }
